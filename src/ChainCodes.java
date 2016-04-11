2b58956260e96141cfe3d8af842d1be3539e0c5a
Kori4r2 <xitsilvax@hotmail.com>
2016-04-11 09:47:21 -0300

    mais erros corigidos, javadoc iniciado, nova imagem adicionada

--- src/ChainCodes.java
+++ src/ChainCodes.java
@@ -2,6 +2,13 @@ import java.io.File;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 
+/**
+* Esta e a classe responsavel por criar um chain code de uma imagem. Esta classe analisa um objeto da classe
+* {@link BufferedImage BufferedImage} para obter o chain code da imagem.
+*
+* @author Italo Tobler Silva - nUSP 8551910
+* @author Vinicius Candiani - nUSP 8551910
+*/
 public class ChainCodes{
 	private int initPoint;
 	private int width;
@@ -10,37 +17,62 @@ public class ChainCodes{
 	private double length;
 	private int[][] imgMatrix;
 
+	/**
+	* Construtor padrao da classe. O construtor analisa a imagem passada e gera o chain codes com todas
+	* as informacoes necessarias.
+	*
+	* @param img - imagem a ser analisada para gerar o chain code.
+	*/
 	public ChainCodes(BufferedImage img){
 		imgMatrix = convertToMatrix(img);
 	}
 
+	/**
+	* Metodo que analisa a imagem e converte seu conteudo para uma matriz de numeros inteiros. Durante a
+	* conversao sao analisados e salvos a altura, a largura e o ponto inicial da imagem.
+	*
+	* @param img - imagem a ser analisada.
+	*/
 	private int[][] convertToMatrix(BufferedImage img){
 		boolean found = false;
-		int h = img.getHeight();
-		int w = img.getWidth();
-		imgMatrix = new int[h][w];
+		height = img.getHeight();
+		width = img.getWidth();
+		int[][] matrix = new int[height][width];
 
 		for(int row = 0; row < height; row++){
 			for(int col = 0; col < width; col++){
-				imgMatrix[row][col] =  (img.getRGB(col, row) & 0xff);
-				if(!found && imgMatrix[row][col] > 0){
+				matrix[row][col] =  (img.getRGB(col, row) & 0xff);
+				if(!found && matrix[row][col] < 255){
 					found = true;
 					initPoint = (width * row) + col;
 				}
 			}
 		}
 
-		return imgMatrix;
+		return matrix;
+	}
+
+	public void printMatrix(){
+		for(int i = 0; i < height; i++){
+			for(int j = 0; j < width; j++){
+				if(imgMatrix[i][j] > 0)
+					System.out.print(" ");
+				else
+					System.out.print(".");
+			}
+			System.out.println("");
+		}
 	}
 
 	public static void main(String[] args){
 		File n = null;
 		try{
-			n = new File("image.png");
+			n = new File("./utils/aform.png");
 		}catch(NullPointerException e){
 			System.out.println(e.getMessage());
 			return;
 		}
+		System.out.println("abriu o arquivo");
 
 		BufferedImage image = null;
 		try{
@@ -49,7 +81,9 @@ public class ChainCodes{
 			System.out.println(e.getMessage());
 			return;
 		}
+		System.out.println("obteve a imagem");
 
 		ChainCodes code = new ChainCodes(image);
+		code.printMatrix();
 	}
 }
